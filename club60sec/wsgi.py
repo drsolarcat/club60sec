ede144491605b07592bc3f67237d12ad93c2272f
Alexey Isavnin <alexey.isavnin@gmail.com>
2016-08-06 00:29:32 +0300

    dev

--- club60sec/wsgi.py
+++ club60sec/wsgi.py
@@ -13,9 +13,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "club60sec.settings.dev")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+# application = DjangoWhiteNoise(application)

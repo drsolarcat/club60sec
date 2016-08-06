7d0c2f920c5739580d8f059f7c6c49f6356ae6b7
Alexey Isavnin <alexey.isavnin@gmail.com>
2016-08-07 00:17:01 +0300

    reverted some changes

--- club60sec/wsgi.py
+++ club60sec/wsgi.py
@@ -13,9 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# from whitenoise.django import DjangoWhiteNoise
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "club60sec.settings.dev")
 
 application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)

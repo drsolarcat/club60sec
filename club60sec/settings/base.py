4142054b4371ef2fd7369d1eb15f8c5b1a70b255
Alexey Isavnin <alexey.isavnin@gmail.com>
2016-08-06 23:15:11 +0300

    deleted obsolete static files

--- club60sec/settings/base.py
+++ club60sec/settings/base.py
@@ -96,11 +96,14 @@ WSGI_APPLICATION = 'club60sec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'club60sec',
+        'USER': 'postgres',
+        'PASSWORD': 'b!gKnife77',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/

1d5d034fb83dbedc02cded99bcf24a1fdbc80139
Alexey Isavnin <alexey.isavnin@gmail.com>
2016-08-07 17:47:16 +0300

    removed v4

--- club60sec/settings/production.py
+++ club60sec/settings/production.py
@@ -28,7 +28,6 @@ STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
 MEDIAFILES_LOCATION = 'media'
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-os.environ['S3_USE_SIGV4'] = 'True'
 
 try:
     from .local import *
